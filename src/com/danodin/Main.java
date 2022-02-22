1bf3ea3efa36e5e7339323cdd0ca578488f1b3de
dpoxo <danphilodin@gmail.com>
2022-02-22 02:35:33 +0000

    Java lessons exercises

---
+++ src/com/danodin/Main.java
@@ -0,0 +1,16 @@
+package com.danodin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Exercise 1.1: \n");
+	    System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun\n");
+
+        System.out.println("Exercise 1.2: \n");
+        for (int i=0; i<5; i++){
+            System.out.println("Java a is cool programming language");
+        }
+    }
+}
